bb78d71a5d50d44f8ac5f16aaa58c6c533b6035b
jesuspestana01 <jesuspestana77@gmail.com>
2022-08-09 23:40:12 -0400

    not longer using scss on the boilerplate. configure modules on webpack and scss files to use .css files instead. improved building times while saving styles sheets

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,10 @@
+import React from "react";
 import ReactDOM from "react-dom/client"; // In order to use React 18 instead of React 17 render. method
 
 // Importing layout to be render with react routers*/
 
 import Layout from "./layout.js";
-
-// Importing bootstrap to our bundle and general styles .scss
-
-import "bootstrap";
-import "../styles/index.scss";
-import React from "react";
+import "../styles/index.css";
 
 ReactDOM.createRoot(
     document.getElementById("root")
